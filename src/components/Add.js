1415fad0b8b320e4b450576ed84d24bf965fd4d5
amal-dev-01 <devmpamal@gmail.com>
2023-06-26 19:58:05 +0530

    first commit

---
+++ src/components/Add.js
@@ -0,0 +1,60 @@
+// import React, { useReducer, useRef } from 'react';
+// import { Button } from 'react-bootstrap';
+
+// const initialState = [];
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case 'ADD_ITEM':
+//       return [
+//         ...state,
+//         {
+//           id: state.length + 1,
+//           name: action.payload,
+//         },
+//       ];
+
+//     case 'DELETE':
+//       return state.filter((d) => d.id !== action.payload);
+
+//     default:
+//       return state;
+//   }
+// };
+
+// const Todo = () => {
+//   const [todos, dispatch] = useReducer(reducer, initialState);
+//   const inputRef = useRef(null);
+
+//   const addItem = () => {
+//     const value = inputRef.current.value;
+//     if (value.trim() !== '') {
+//       dispatch({ type: 'ADD_ITEM', payload: value });
+//       inputRef.current.value = ''; // Clear the input box
+//     }
+//   };
+
+//   return (
+//     <>
+//       <div>
+//         <h1>Todo App</h1>
+//       </div>
+//       <div>
+//         <input type="text" ref={inputRef} />
+//         <button onClick={addItem}>Add</button>
+//       </div>
+//       <div>
+//         {todos.map((todo) => (
+//           <li key={todo.id} style={{ listStyle: 'none' }}>
+//             {todo.name}
+//             <Button
+//               variant="danger"
+//               onClick={() => dispatch({ type: 'DELETE', payload: todo.id })}
+//             >
+//               Delete
+//             </Button>
+//           </li>
+//         ))}
+//       </div>
+//     </>
+//   );
+// };
